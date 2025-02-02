418a51fc4b3a542f220320ad93a2d318a1da7a5a
Kandy-Hamisi <hkandy2000@gmail.com>
2025-02-02 23:57:26 +0300

    setup section design

--- sections/website/Responsiveness.tsx
+++ sections/website/Responsiveness.tsx
@@ -7,6 +7,18 @@ const Responsiveness = () => {
         <h4 className="wide:text-4xl green-text-gradient font-bold">
           Better design for content
         </h4>
+        <p className="wide:text-lg text-sm text-center mt-8 wide:leading-[35px] leading-[30px]">
+          Startup Framework contains components and complex blocks which can
+          easily be integrated into almost any design. All of the components are
+          made in the same style, and can easily be integrated into projects,
+          allowing you to create hundreds of solutions.
+        </p>
+      </div>
+      <div className="flex items-center gap-12">
+        <div>
+          <h6>Some info here</h6>
+        </div>
+        <div className="relative"></div>
       </div>
     </section>
   );
